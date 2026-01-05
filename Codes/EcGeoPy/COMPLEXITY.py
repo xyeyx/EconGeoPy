0a382d6ea29f07d9e89d379826464c1d20da8b3d
xyeyx <77643684+xyeyx@users.noreply.github.com>
2026-01-05 10:23:09 +0100

    Update 20260105 002

--- Codes/EcGeoPy/COMPLEXITY.py
+++ Codes/EcGeoPy/COMPLEXITY.py
@@ -5,7 +5,7 @@
 import numpy as np
 from .RCA import rca
 
-def rel_asymmetric(mat:np.ndarray) -> np.ndarray:
+def rel_asymmetric(mat:np.ndarray, weight:np.ndarray|None = None) -> np.ndarray:
     '''Hidalgo's asymetric version of relatedness between two items, based on
        the conditional probability of having both items i and j, conditioning
        on already having i. Namely...
@@ -19,17 +19,32 @@ def rel_asymmetric(mat:np.ndarray) -> np.ndarray:
             has RCA (i.e. 1/0) in each product/task. 
             Row: Product/Task/ etc.
             Col: Region
+    
+       weight: numpy 1-d array.  Optional. 
+               Importance weight of each region (e.g. population, gdp size, etc). 
+               Must be positive and dimension corresponds to the number of regions in mat.
     '''
+    if weight is not None:
+        if weight.ndim>1:
+            raise ValueError("If 'weight' is supplied, it must be an 1-d array with the same number of elements like the number of columns of 'mat', but currently its dimension is {}.".format(weight.ndim));
+        if len(weight) != mat.shape[1]:
+            raise ValueError("'weight' must have the same number of elements like the number of columns of 'mat', but currently it has {} elements.".format(weight.ndim));
+        if weight.min()<=0:
+            raise ValueError("'weight' must be positive.");
+    
+    if weight is None:
+        weight = 1;
+    
     hasRCA = (mat>=1.0).astype(int);
-    denominator = hasRCA.sum(axis = 1, keepdims = True);
+    denominator = (hasRCA * weight).sum(axis = 1, keepdims = True);
     denominator[denominator == 0] = 10086;
-    numerator = np.matmul(hasRCA, hasRCA.T);
+    numerator = np.matmul(hasRCA * weight, hasRCA.T);
     Phi_asym = numerator / denominator;
     np.fill_diagonal(Phi_asym, 0);
     return Phi_asym;
 
 
-def rel_symmetric(mat:np.ndarray) -> np.ndarray:
+def rel_symmetric(mat:np.ndarray, weight:np.ndarray|None = None) -> np.ndarray:
     '''Hidalgo's symetric version of relatedness between two items,
        phi(i,j) = min( phi(i->j), phi(j->i) )
 
@@ -39,8 +54,12 @@ def rel_symmetric(mat:np.ndarray) -> np.ndarray:
             has RCA (i.e. 1/0) in each product/task. 
             Row: Product/Task/ etc.
             Col: Region
+       
+       weight: numpy 1-d array.  Optional. 
+            Importance weight of each region (e.g. population, gdp size, etc). 
+            Must be positive and dimension corresponds to the number of regions in mat.
     '''
-    Phi_asym = rel_asymmetric(mat);
+    Phi_asym = rel_asymmetric(mat, weight);
     return np.minimum(Phi_asym, Phi_asym.T);
 
 
@@ -104,7 +123,8 @@ def stijn_normalization(co_occur_mat:np.ndarray) -> np.ndarray:
 
 def relatedness(mat:np.ndarray, 
                 input_type:str = 'Export',
-                method: str = 'Symmetric') -> np.ndarray:
+                method: str = 'Symmetric',
+                weight: np.ndarray|None = None) -> np.ndarray:
     '''
     Generate the relatedness matrix based on co-occurance of revealed
     comparative advantage. See eg. Hildalgo et al (2007) "The Product Space".
@@ -130,6 +150,10 @@ def relatedness(mat:np.ndarray,
             - Association => Association strength
             - Jaccard => Jaccard similarity
             - Steijn => Steijn's probability measure
+    
+    weight: numpy 1-d array.  Optional. 
+            Importance weight of each region (e.g. population, gdp size, etc). 
+            Must be positive and dimension corresponds to the number of regions in mat.
     '''
     if mat.ndim != 2:
         raise ValueError("'mat' must be a 2-d array, but currently its dimension is {}.".format(mat.ndim));
@@ -149,16 +173,27 @@ def relatedness(mat:np.ndarray,
     if input_type not in allowed_types:
         raise ValueError("'input_type' must be one of: {}.".format("', '".join(allowed_types)));
     
+    if weight is not None:
+        if weight.ndim>1:
+            raise ValueError("If 'weight' is supplied, it must be an 1-d array with the same number of elements like the number of columns of 'mat', but currently its dimension is {}.".format(weight.ndim));
+        if len(weight) != mat.shape[1]:
+            raise ValueError("'weight' must have the same number of elements like the number of columns of 'mat', but currently it has {} elements.".format(weight.ndim));
+        if weight.min()<=0:
+            raise ValueError("'weight' must be positive.");
+        
+    
     if input_type == 'Export':
         mat = rca(mat);
     
     if method == "Asymmetric":
-        Result = rel_asymmetric(mat);
+        Result = rel_asymmetric(mat, weight);
     elif method == "Symmetric":
-        Result = rel_symmetric(mat);
+        Result = rel_symmetric(mat, weight);
     else:
         hasRCA = (mat>=1.0).astype(int);
-        co_occur_counts = np.matmul(hasRCA, hasRCA.T);
+        if weight is None:
+            weight = 1;
+        co_occur_counts = np.matmul(hasRCA*weight, hasRCA.T);
         np.fill_diagonal(co_occur_counts, 0);
         if method == "Jaccard":
             Result = jaccard_normalization(co_occur_counts);
