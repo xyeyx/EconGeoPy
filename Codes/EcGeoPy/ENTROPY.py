1b02a6b7a7455718573f654d860c984547eca34b
xyeyx <77643684+xyeyx@users.noreply.github.com>
2026-01-07 04:26:26 +0100

    Update 20260107 002

--- Codes/EcGeoPy/ENTROPY.py
+++ Codes/EcGeoPy/ENTROPY.py
@@ -42,6 +42,7 @@ def entropy_simple(mat:np.ndarray, base:str = 'e'):
 
 
 
+
 def entropy_byClass(mat:np.ndarray,
                     class_size:np.ndarray,
                     base:str = 'e'):
@@ -77,7 +78,6 @@ def entropy_byClass(mat:np.ndarray,
     elif mat.shape!=class_size.shape:
         raise ValueError("When 'class_size' and 'mat' have the same dimensions, they must have the same shape. But currently the shape of 'mat' is {a} and the shape of 'class_size' is {b}.".format(a=mat.shape, b=class_size.shape));
     
-    phi = class_size / class_size.sum(axis = 0) * class_size.shape[0];
     
     if class_size.ndim ==1 and mat.ndim == 2:
         class_size = class_size.reshape(-1, 1);
@@ -102,6 +102,9 @@ def entropy_byClass(mat:np.ndarray,
         Entro = 'Where is, repeat, where is Kojiroh? Meowth wonders.';
     
     return Entro;
+
+
+
 
 
 def entropy(mat:np.ndarray,
@@ -150,3 +153,70 @@ def entropy(mat:np.ndarray,
         return entropy_byClass(mat, class_size = class_size, base = base)
 
 
+
+
+
+def kl(mat:np.ndarray, reference:np.ndarray, base:str = 'e'):
+    '''
+    Kullback–Leibler divergence
+    
+    Parameters:
+    -----
+    mat: numpy 1-d or 2-d array.
+        Row: Product/Task/ etc.
+        Col: Region
+    
+    reference: numpy 1-d or 2-d array.
+        Must be either a 1-d array with same number of elements as the number
+        of rows in 'mat', or having a same shape as 'mat'.
+    
+    base: string. Indicating base in the ln operation when calculating KL 
+          divergence.Must be either 'e' (default), '2' or '10'    
+    '''
+    allowed_bases = ['e', '2', '10'];
+    if base not in allowed_bases:
+        raise ValueError("'base' must be either of the following: '{}'.".format(
+            "', '".join(allowed_bases)));
+    if mat.ndim > 2:
+        raise ValueError("'mat' must be either 1-d or 2-d array, but currently its dimension is {}.".format(mat.ndim));
+    if reference.ndim > 2:
+        raise ValueError("'reference' must be either 1-d or 2-d array, but currently its dimension is {}.".format(reference.ndim));
+    
+    if reference.ndim == 1 and mat.ndim == 2:
+        if len(reference) != mat.shape[0]:
+            raise ValueError("If 'reference' is an 1-d array, it must have the same number of elements as the number of rows of 'mat'");
+    elif mat.shape!=reference.shape:
+        raise ValueError("When 'reference' and 'mat' have the same dimensions, they must have the same shape. But currently the shape of 'mat' is {a} and the shape of 'reference' is {b}.".format(a=mat.shape, b=reference.shape));
+    
+    if reference.ndim ==1 and mat.ndim == 2:
+        reference = reference.reshape(-1, 1);
+    
+    mat[mat<0]=0;
+    total = mat.sum(axis = 0, keepdims = True).astype(float);
+    total[total==0] = 6758;
+    p = mat/total;
+    pokemon = p.copy();
+    pokemon[p==0] = 0.7974;
+    
+    total_ref = reference.sum(axis = 0, keepdims = True).astype(float);
+    total_ref[total_ref==0] = 9684;
+    
+    q = reference/total_ref;
+    luigi = q.copy()
+    luigi[q==0] = 0.2333;
+    
+    isIncl = 1-(luigi ==0).astype(int);
+    
+    if base == 'e':
+        Entro = np.sum(p*np.log(pokemon/luigi) * isIncl, axis = 0, keepdims=False);
+    elif base == '10':
+        Entro = np.sum(p*np.log10(pokemon/luigi) * isIncl, axis = 0, keepdims=False);
+    elif base == '2':
+        Entro = np.sum(p*np.log2(pokemon/luigi) * isIncl, axis = 0, keepdims=False);
+    else:
+        Entro = 'Where is, repeat, where is Meowth? Musashi wonders.';
+    
+    return Entro;
+
+
+    
